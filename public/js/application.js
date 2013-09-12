d524fe5426d04813546eca61704ba2419e6a023d
Michael Rhee <mjrhee@gmail.com>
2013-09-12 15:29:56 -0500

    Work on javascript syllable counter

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,42 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$(document).ready(function() {
+
+  function countSyllables(word){
+    
+    var processed = word;
+    var suffix_bonus = 0;
+    return processed;
+  }
+
+  function returnCount(word, count) {
+    return count - (word.length)
+  }
+
+  function countSyllable(word) {
+    var consonants = "bcdfghjklmnpqrstvwxz";
+    var vowels = "aeiouy";
+    var processed = word;
+    var vowelCombos = new RegExp("iou|eau|ai|au|ay|ea|ee|ei|oa|oi|oo|ou|ui|oy");
+    var consCombos = new RegExp("qu|ng|ch|rt|[#{" + consonants + "}h]/")
+    word = word.replace(vowelCombos, "@");
+    word = word.replace(consCombos, "=");
+    return word
+  }
+
+  $("#lineOne").keyup(function(){
+    var lineOne = $(this).val();
+    $("#lineOneSCount").html("<p>Syllables Remaining: " + returnCount(lineOne, 7) + "</p>");
+  });
+
+  $("#lineTwo").keyup(function(){
+    var lineTwo = $(this).val();
+    $("#lineTwoSCount").html("<p>Syllables Remaining: " + countSyllable(lineTwo) + "</p>");
+  });
+
+
+
+
+
+
 });
