aed8ef3c3cab2aec9b3ef4df85e29be6b3bf5353
Michael Rhee <mjrhee@gmail.com>
2013-09-13 12:39:34 -0500

    Add Maiku tweeting

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,14 +3,14 @@
 get '/' do
   random = rand(1..Haiku.count)
   @haiku = Haiku.find(random)
-  if @haiku.author[0] == "@"
-    @author = @haiku.author
-    @link = "http://twitter.com/#{@haiku.author[1..-1]}"
-  elsif @haiku.author != ""
-    @author = @haiku.author
-  else
-    @author = "anonymous"
-  end
+  # if @haiku.author[0] == "@"
+  #   @author = @haiku.author
+  #   @link = "http://twitter.com/#{@haiku.author[1..-1]}"
+  # elsif @haiku.author != ""
+  #   @author = @haiku.author
+  # else
+  #   @author = "anonymous"
+  # end
   erb :index
 end
 
@@ -18,5 +18,11 @@ end
 
 post '/create' do
   Haiku.create(params)
+  tweet = <<-TWEET
+#{params[:line_one]}
+#{params[:line_two]}
+#{params[:line_three]}
+  TWEET
+  Twitter.update(tweet)
   redirect '/'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,9 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'oauth'
+require 'twitter'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
@@ -31,3 +34,16 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+  config.oauth_token = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+end
