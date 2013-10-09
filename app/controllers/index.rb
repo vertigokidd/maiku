3006886ef21e6a6edb2e0b28f582b82ace939218
Michael Rhee <mjrhee@gmail.com>
2013-10-09 08:58:03 -0500

    Refactor auth controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,15 +18,10 @@ end
 get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-  # at this point in the code is where you'll need to create your user account and store the access token
-  
-  user_token = @access_token.params[:oauth_token]
-  user_secret = @access_token.params[:oauth_token_secret]
 
-  tweeter = Twitter::Client.new(:oauth_token => user_token,
-                                :oauth_token_secret => user_secret
+  tweeter = Twitter::Client.new(:oauth_token => @access_token.params[:oauth_token],
+                                :oauth_token_secret => @access_token.params[:oauth_token_secret]
                                 )
   tweeter.update(session[:tweet] + "(made with @maikuapp)")
 
